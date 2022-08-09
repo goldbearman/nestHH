41530f99b176e329d43ca8774ee02bdc0b5f0028
fsabirov <sabirovfarit@gmail.com>
2022-08-09 16:23:34 +0300

    end of the first stage

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import {Controller, Post, HttpCode, Body} from '@nestjs/common';
+import {AuthDto} from "./dto/auth.dto";
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+    @Post('register')
+    async register(@Body() dto:AuthDto){
+
+    }
+    @HttpCode(200)
+    @Post('login')
+    async login(@Body() dto:AuthDto){
+
+    }
+}
